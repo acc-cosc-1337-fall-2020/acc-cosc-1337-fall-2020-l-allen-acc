1c71497938dd9eb7deb7ebe629135e4b385f017f
Leonard Allen <44595601+ljallenii@users.noreply.github.com>
2020-09-02 15:51:08 -0500

    Finished Homework Assignment 1

--- src/classwork/01_assign/main.cpp
+++ src/classwork/01_assign/main.cpp
@@ -1,11 +1,12 @@
 //Write the include statement for types.h here
-
+#include "types.h"
 //Write include for capturing input from keyboard and displaying output to screen
-
+#include <iostream>
 
 
 int main() 
 {
+	std::cout << sum_numbers(5 ,10);
 
 	return 0;
 }
--- src/homework/01_variables/main.cpp
+++ src/homework/01_variables/main.cpp
@@ -1,15 +1,15 @@
 ﻿//write include statements
-
-
+#include <iostream>
+#include "variables.h"
 //write namespace using statement for cout
-
+using namespace std;
 
 /*
 Call multiply_numbers with 10 and 10 parameter values and display function result
 */
 int main()
 {
-	
+	cout << multiply_numbers(10, 10) << endl;	
 
 	return 0;
 }
