1ccb88b39aca26a1d3bd74ab47a2d98719040b1b
Leonard Allen <44595601+ljallenii@users.noreply.github.com>
2020-10-02 16:18:52 -0500

    Homework 3 Submission

--- src/classwork/04_assign/loops.cpp
+++ src/classwork/04_assign/loops.cpp
@@ -1,1 +1,15 @@
 //cpp
+#include <iostream>
+
+/* Factorial takes a numer (starting at 1), multiplies itself by the next number incrementing order
+   all the way to the target number */
+
+int factorial(int num)
+{
+    int answer = 1;     //  Initialize the answer
+    for(int x=1; x<=num; x++)       //  Loop until the number that increments reaches the target number
+    {
+        answer = answer * x;        //  Multiply the incrementing number to the answer
+    } 
+    return answer;          //  Return answer
+}
--- src/classwork/04_assign/main.cpp
+++ src/classwork/04_assign/main.cpp
@@ -1,7 +1,10 @@
 //write includes statements
+#include <iostream>
+#include "loops.h"
+#include <cctype>
 
 //write using statements for cin and cout
-
+using std::cout;	using std::cin;
 
 /*
 Use a do while loop to prompt the user for 
@@ -10,6 +13,28 @@ factorial.  Also, loop continues as long as user wants to.
 */
 int main() 
 {
+	std::string option;
+	int number;		//  Initialize the number once
+	do
+	{
+		cout << "\nEnter a number between 1 and 20: \t";
+		cin >> number;											//  User input needed for number that you want factorialized
+		while(number < 1 || number > 20)		//  If number is less than on or exceeds 20, then error will be posted 
+		{
+			cout << "\nERROR! Number must be within number range!" << std::endl;
+			cout << "Please enter a number between 1 and 20: \t";
+			cin >> number;
+		}
 
+		int answer = factorial(number);		//  Initialize "answer" with the factorrial product
+
+		cout << "\nThe factorial of " << number << " is: " << answer << std::endl;		//  Prints factorial product
+
+		cout << "Would you like to continue? (input 'Y' or 'y' to repeat)\t";		//  Asks the user if they would like to try another number
+		cin >> option;																//  If yes then program will loop
+	} while (option == "Y" || option == "y");
+	
 	return 0;
 }
+
+//  How would you write this with an unsigned long long? My numbers were still inaccurate every time.
